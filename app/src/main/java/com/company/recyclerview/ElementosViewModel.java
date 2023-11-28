113f9fe9d0ff975c6571bfa0130c6d1cf42c324c
laura1513 <laurajorques2003@gmail.com>
2023-11-28 16:40:30 +0100

    practica guiada

--- app/src/main/java/com/company/recyclerview/ElementosViewModel.java
+++ app/src/main/java/com/company/recyclerview/ElementosViewModel.java
@@ -14,6 +14,8 @@ public class ElementosViewModel extends AndroidViewModel {
 
     MutableLiveData<List<Elemento>> listElementosMutableLiveData = new MutableLiveData<>();
 
+    MutableLiveData<Elemento> elementoSeleccionado = new MutableLiveData<>();
+
     public ElementosViewModel(@NonNull Application application) {
         super(application);
 
@@ -51,5 +53,12 @@ public class ElementosViewModel extends AndroidViewModel {
                 listElementosMutableLiveData.setValue(elementos);
             }
         });
+    }
+    void seleccionar(Elemento elemento){
+        elementoSeleccionado.setValue(elemento);
+    }
+
+    MutableLiveData<Elemento> seleccionado(){
+        return elementoSeleccionado;
     }
 }
